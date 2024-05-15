65ec31bca43700bfba50d4f43f7af993e6b18a41
Alex <alex.d.nax@gmail.com>
2024-05-15 18:19:13 +0200

    add modal whitelist type; fix deselect on add new address; open add address modal on empty address array; add whitelist mock for eth whitelist addresses
    fix layout; add ens&whitelist query; fix texting

--- spaceward/src/components/SendEth.tsx
+++ spaceward/src/components/SendEth.tsx
@@ -9,10 +9,9 @@ import { SignMethod } from "warden-protocol-wardenprotocol-client-ts/lib/warden.
 import { useQueryHooks } from "@/hooks/useClient";
 import { AddressType } from "@wardenprotocol/wardenjs/codegen/warden/warden/v1beta2/key";
 import { ArrowUpRight } from "lucide-react";
+import { getProvider } from "@/lib/eth";
 
-const url = "https://ethereum-sepolia-rpc.publicnode.com";
-
-const provider = new ethers.JsonRpcProvider(url);
+const provider = getProvider("sepolia");
 
 async function buildEthTransaction(
 	chainId: string | number,
